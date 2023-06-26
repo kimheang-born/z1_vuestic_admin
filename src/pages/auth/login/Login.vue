a32793467700bf22d3fa09dabf1d8a9205cfbc30
Kimheang <kimheang135@gmail.com>
2023-06-26 11:20:09 +0700

    feat: add condition when login

--- src/pages/auth/login/Login.vue
+++ src/pages/auth/login/Login.vue
@@ -1,37 +1,53 @@
 <template>
-  <form @submit.prevent="onsubmit">
-    <va-input
-      v-model="email"
-      class="mb-3"
-      type="email"
-      :label="t('auth.email')"
-      :error="!!emailErrors.length"
-      :error-messages="emailErrors"
-    />
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <form @submit.prevent="submitForm">
+      <!-- <va-input
+        v-model="email"
+        class="mb-3"
+        type="email"
+        :label="t('auth.email')"
+        :error="!!emailErrors.length"
+        :error-messages="emailErrors"
+      />
+  
+      <va-input
+        v-model="password"
+        class="mb-3"
+        type="password"
+        :label="t('auth.password')"
+        :error="!!passwordErrors.length"
+        :error-messages="passwordErrors"
+      />
+  
+      <div class="auth-layout__options d-flex align-center justify-space-between">
+        <va-checkbox v-model="keepLoggedIn" class="mb-0" :label="t('auth.keep_logged_in')" />
+        <router-link class="ml-1 va-link" :to="{ name: 'recover-password' }">{{
+          t('auth.recover_password')
+        }}</router-link>
+      </div>
+  
+      <div class="d-flex justify-center mt-3">
+        <va-button class="my-0" @click="onsubmit">{{ t('auth.login') }}</va-button>
+      </div> -->
 
-    <va-input
-      v-model="password"
-      class="mb-3"
-      type="password"
-      :label="t('auth.password')"
-      :error="!!passwordErrors.length"
-      :error-messages="passwordErrors"
-    />
+      <va-input v-model="phoneNumber" class="mb-3" type="text" :label="t('auth.phoneNumber')" />
 
-    <div class="auth-layout__options d-flex align-center justify-space-between">
-      <va-checkbox v-model="keepLoggedIn" class="mb-0" :label="t('auth.keep_logged_in')" />
-      <router-link class="ml-1 va-link" :to="{ name: 'recover-password' }">{{
-        t('auth.recover_password')
-      }}</router-link>
-    </div>
+      <va-input v-model="password" class="mb-3" type="password" :label="t('auth.password')" />
 
-    <div class="d-flex justify-center mt-3">
-      <va-button class="my-0" @click="onsubmit">{{ t('auth.login') }}</va-button>
-    </div>
-  </form>
+      <div class="d-flex justify-center mt-3">
+        <va-button class="my-0" @click="submitForm">{{ t('auth.login') }}</va-button>
+      </div>
+    </form>
+  </div>
 </template>
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
   import { computed, ref } from 'vue'
   import { useRouter } from 'vue-router'
   import { useI18n } from 'vue-i18n'
@@ -53,5 +69,44 @@
     passwordErrors.value = password.value ? [] : ['Password is required']
 
     router.push({ name: 'dashboard' })
+  }
+</script> -->
+
+<script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { useStore } from 'vuex'
+  import { useI18n } from 'vue-i18n'
+
+  const store = useStore()
+  const { t } = useI18n()
+
+  const phoneNumber = ref('+855962424486')
+  const password = ref('1234')
+  const isLoading = ref(false)
+  const error = ref(null)
+  const router = useRouter()
+
+  const actionPayload = {
+    phone: phoneNumber.value,
+    password: password.value,
+  }
+
+  const submitForm = async function () {
+    try {
+      isLoading.value = true
+
+      await store.dispatch('login', actionPayload)
+
+      router.push({ name: 'dashboard' })
+    } catch (err) {
+      error.value = err?.message || 'Failed to authenticate, try later.'
+    }
+
+    isLoading.value = false
+  }
+
+  const handleError = function () {
+    error.value = null
   }
 </script>
