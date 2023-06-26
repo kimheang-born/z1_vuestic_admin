3de2fbbf8019dae1a14302d76781f56ce3fe6d81
Kimheang <kimheang135@gmail.com>
2023-06-26 11:19:17 +0700

    feat: add new components

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,28 @@ import i18n from './i18n'
 import { createVuestic } from 'vuestic-ui'
 import { createGtm } from '@gtm-support/vue-gtm'
 
-// import stores from './stores' // pinia
+import stores from './stores' // pinia
 import store from './store' // vuex
 import router from './router'
 import vuesticGlobalConfig from './services/vuestic-ui/global-config'
 import App from './App.vue'
 
+// Components
+import BaseButton from './components/ui/BaseButton.vue'
+import BaseDialog from './components/ui/BaseDialog.vue'
+import BaseSpinner from './components/ui/BaseSpinner.vue'
+
 const app = createApp(App)
 
-// app.use(stores) // pinia
+app.use(stores) // pinia
 app.use(store) // vuex
 app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
+
+app.component('BaseButton', BaseButton)
+app.component('BaseDialog', BaseDialog)
+app.component('BaseSpinner', BaseSpinner)
 
 if (import.meta.env.VITE_APP_GTM_ENABLED) {
   app.use(
